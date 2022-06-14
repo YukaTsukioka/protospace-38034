f3d5b134d9566eb36c94f5a53a6aef17200b7464
YukaTsukioka <yuka.tsukioka720@gmail.com>
2022-06-14 14:42:33 +0900

    一覧表示されている画像をクリックすると、該当する詳細ページへ遷移するよう編集

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, only: [:edit, :destroy]
-  before_action :move_to_index, except: [:index]
-
+  
 
 def index
   @prototypes = Prototype.all
@@ -53,10 +52,5 @@ def  prototype_params
   params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
 end
 
-def move_to_index
-  unless user_signed_in?
-    redirect_to action: :index
-  end
-end
 
 end
